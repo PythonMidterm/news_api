9c13423b1e2a06e768051d4e0fc4f9691d3a2044
MADELINE PETERS <madelinepet@hotmail.com>
2018-09-10 12:25:24 -0700

    Worked with Roman, created conftest and test_intigration and db for testing called news_test.

--- news_api/tests/test_integration.py
+++ news_api/tests/test_integration.py
@@ -1,40 +1,77 @@
-import requests
-import unittest
-from pyramid import testing
-import transaction
+# import requests
+# import unittest
+# from pyramid import testing
+# import transaction
 
 
+# def dummy_request(dbsession):
+#     return testing.DummyRequest(dbsession=dbsession)
 
-def dummy_request(dbsession):
-    return testing.DummyRequest(dbsession=dbsession)
+
+# class BaseTest(unittest.TestCase):
+#     def setUp(self):
+#         self.config = testing.setUp(settings={
+#             'sqlalchemy.url': 'postgres://localhost:5432/news_test'
+#         })
+#         self.config.include('.models')
+#         settings = self.config.get_settings()
+
+#         from .models import (
+#             get_engine,
+#             get_session_factory,
+#             get_tm_session,
+#             )
+
+#         self.engine = get_engine(settings)
+#         session_factory = get_session_factory(self.engine)
+
+#         self.session = get_tm_session(session_factory, transaction.manager)
+
+#     def init_database(self):
+#         from .models.meta import Base
+#         Base.metadata.create_all(self.engine)
+
+#     def tearDown(self):
+#         from .models.meta import Base
+
+#         testing.tearDown()
+#         transaction.abort()
+#         Base.metadata.drop_all(self.engine)
+import json
 
 
-class BaseTest(unittest.TestCase):
-    def setUp(self):
-        self.config = testing.setUp(settings={
-            'sqlalchemy.url': 'postgres://localhost:5432/weather_test'
-        })
-        self.config.include('.models')
-        settings = self.config.get_settings()
+def test_registration(testapp):
+    """
+    """
+    account = {
+        'email': 'test@example.com',
+        'password': 'hello',
+    }
 
-        from .models import (
-            get_engine,
-            get_session_factory,
-            get_tm_session,
-            )
+    response = testapp.post('/api/v1/auth/register', json.dumps(account))
+    assert response.status_code == 201
+    assert response.json['token']
 
-        self.engine = get_engine(settings)
-        session_factory = get_session_factory(self.engine)
 
-        self.session = get_tm_session(session_factory, transaction.manager)
+def test_invalid_registration(testapp):
+    """
+    """
+    account = {
+        'email': 'test_two@example.com',
+    }
 
-    def init_database(self):
-        from .models.meta import Base
-        Base.metadata.create_all(self.engine)
+    response = testapp.post('/api/v1/auth/register', json.dumps(account), status='4**')
+    assert response.status_code == 400
 
-    def tearDown(self):
-        from .models.meta import Base
 
-        testing.tearDown()
-        transaction.abort()
-        Base.metadata.drop_all(self.engine)
+def test_login(testapp):
+    """
+    """
+    account = {
+        'email': 'test@example.com',
+        'password': 'hello',
+    }
+
+    response = testapp.post('/api/v1/auth/login', json.dumps(account))
+    assert response.status_code == 201
+    assert response.json['token']
