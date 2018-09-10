ca3a8e091543492e5415b9b057faae1ef6219b60
MADELINE PETERS <madelinepet@hotmail.com>
2018-09-10 14:06:56 -0700

    Testing in progress on auth route.

--- news_api/tests/test_integration.py
+++ news_api/tests/test_integration.py
@@ -41,37 +41,77 @@ import json
 
 
 def test_registration(testapp):
-    """
+    """ Tests new user registration returns a 201 status code
     """
     account = {
         'email': 'test@example.com',
         'password': 'hello',
     }
 
-    response = testapp.post('/api/v1/auth/register', json.dumps(account))
+    response = testapp.post('/api/v1/auth/register/', json.dumps(account))
     assert response.status_code == 201
     assert response.json['token']
 
 
 def test_invalid_registration(testapp):
-    """
+    """ Tests invalid registration returns 400 status code
     """
     account = {
         'email': 'test_two@example.com',
     }
 
-    response = testapp.post('/api/v1/auth/register', json.dumps(account), status='4**')
+    response = testapp.post('/api/v1/auth/register/', json.dumps(account), status='4**')
     assert response.status_code == 400
 
 
 def test_login(testapp):
-    """
+    """ Tests login route returns 201 status code
     """
     account = {
         'email': 'test@example.com',
         'password': 'hello',
     }
 
-    response = testapp.post('/api/v1/auth/login', json.dumps(account))
+    response = testapp.post('/api/v1/auth/login/', json.dumps(account))
     assert response.status_code == 201
     assert response.json['token']
+
+
+def test_preferences_lookup(testapp):
+    """ Tests to have user look up by preferences
+    """
+    preference_order = {
+        'preference_order': 'test@example.com',
+    }
+    response = testapp.post('/api/v1/preferences/', json.dumps(preference_order))
+    assert response.status_code == 201
+
+
+# def test_invalid_lookup_methods(testapp):
+#     """
+#     """
+#     response = testapp.put('/api/v1/lookup/98109', status='4**')
+#     assert response.status_code == 405
+#     response = testapp.delete('/api/v1/lookup/98109', status='4**')
+#     assert response.status_code == 405
+#     response = testapp.post('/api/v1/lookup/98109', status='4**')
+#     assert response.status_code == 405
+
+
+# def test_create_location(testapp):
+#     """
+#     """
+#     account = {
+#         'email': 'test@example.com',
+#         'password': 'hello',
+#     }
+
+#     token = testapp.post('/api/v1/auth/login', json.dumps(account)).json['token']
+
+#     location = {
+#         'name': 'Seattle',
+#         'zip_code': 98109
+#     }
+#     testapp.authorization = ('Bearer', token)
+#     response = testapp.post('/api/v1/location', json.dumps(location))
+#     assert response.status_code == 201
