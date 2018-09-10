3576aa35e133c40505a765f9c780d340b980dd63
MADELINE PETERS <madelinepet@hotmail.com>
2018-09-10 14:25:03 -0700

    Four passing tests on the registration, invalid registration, login, and preferences lookup.

--- news_api/tests/test_integration.py
+++ news_api/tests/test_integration.py
@@ -1,43 +1,6 @@
-# import requests
-# import unittest
-# from pyramid import testing
-# import transaction
-
-
-# def dummy_request(dbsession):
-#     return testing.DummyRequest(dbsession=dbsession)
-
-
-# class BaseTest(unittest.TestCase):
-#     def setUp(self):
-#         self.config = testing.setUp(settings={
-#             'sqlalchemy.url': 'postgres://localhost:5432/news_test'
-#         })
-#         self.config.include('.models')
-#         settings = self.config.get_settings()
-
-#         from .models import (
-#             get_engine,
-#             get_session_factory,
-#             get_tm_session,
-#             )
-
-#         self.engine = get_engine(settings)
-#         session_factory = get_session_factory(self.engine)
-
-#         self.session = get_tm_session(session_factory, transaction.manager)
-
-#     def init_database(self):
-#         from .models.meta import Base
-#         Base.metadata.create_all(self.engine)
-
-#     def tearDown(self):
-#         from .models.meta import Base
-
-#         testing.tearDown()
-#         transaction.abort()
-#         Base.metadata.drop_all(self.engine)
 import json
+
+token = ''
 
 
 def test_registration(testapp):
@@ -49,6 +12,8 @@ def test_registration(testapp):
     }
 
     response = testapp.post('/api/v1/auth/register/', json.dumps(account))
+    global token
+    token = response.json['token']
     assert response.status_code == 201
     assert response.json['token']
 
@@ -83,6 +48,8 @@ def test_preferences_lookup(testapp):
     preference_order = {
         'preference_order': 'test@example.com',
     }
+    global token
+    testapp.authorization = ('Bearer', token)
     response = testapp.post('/api/v1/preferences/', json.dumps(preference_order))
     assert response.status_code == 201
 
