acd5928e2351c140db2fca2d5a4047c70b8830e7
MADELINE PETERS <madelinepet@hotmail.com>
2018-09-12 13:40:05 -0700

    Debugging unique add to archives in progress.

--- news_api/utils/scheduler.py
+++ news_api/utils/scheduler.py
@@ -9,6 +9,7 @@ import goose3
 import requests
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import IntegrityError
 
 
 def connect_to_db(db_path):
@@ -59,14 +60,14 @@ def analyze_text(text):
 
 
 def job():
-    """Job to be scheduled for 3-step News Fetch/Extraction/Analyze. 
-    We can trigger at a specified interval (24-hour for demo purposes. 
+    """Job to be scheduled for 3-step News Fetch/Extraction/Analyze.
+    We can trigger at a specified interval (24-hour for demo purposes.
     1-hr or less in true production)
     """
 
-    # db_path = 'postgres://localhost:5432/news_api'
+    db_path = 'postgres://localhost:5432/news_api'
 
-    db_path = 'postgres://roman:password@localhost:5432/news_api'
+    # db_path = 'postgres://roman:password@localhost:5432/news_api'
 
     session = connect_to_db(db_path)
 
@@ -119,10 +120,12 @@ def job():
             try:
                 article_to_insert = Feed(title=article['title'], description=article['description'], source=article['source'], date_published=article['date_published'], url=article['url'], dom_tone=article['dom_tone'], image=article['image'])
                 article_to_insert_archive = Archives(title=article['title'], description=article['description'], source=article['source'], date_published=article['date_published'], url=article['url'], dom_tone=article['dom_tone'], image=article['image'])
-                session.add(article_to_insert)
-                session.add(article_to_insert_archive)
 
-            except(TypeError, KeyError):
+                session.add(article_to_insert)
+                session.add(article_to_insert_archive).on_conflict_do_nothing(
+                    index_elements=['title'])
+
+            except TypeError:
                 continue
 
-    session.commit()
+        session.commit()
